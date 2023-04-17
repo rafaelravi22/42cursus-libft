ac941f803f5e35d2ec926e9e070717216634f461
rafaelravi22 <130813900+rafaelravi22@users.noreply.github.com>
2023-04-17 21:31:40 +0100

    Add files via upload

---
+++ libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafamart <rafamart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 21:21:08 by rafamart          #+#    #+#             */
+/*   Updated: 2023/04/17 21:30:19 by rafamart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*dest;
+
+	dest = malloc(nmemb * size);
+	if (dest == NULL)
+		return (0);
+	ft_memset(dest, 0, nmemb * size);
+	return (dest);
+}
